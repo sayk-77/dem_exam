f0719370747a402ffabb65e37803fa223e395df3
Nikita <yawaihv1@gmail.com>
2023-12-15 02:12:13 +0300

    add userWindow and guestWindow, fix filter

---
+++ DemExam/GuestWindow.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace DemExam
+{
+    public partial class GuestWindow : Form
+    {
+        public GuestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DemExam/UserWindow.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace DemExam
+{
+    public partial class UserWindow : Form
+    {
+        public UserWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
